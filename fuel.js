e6f2869a6be3383e7e6e470d719fd2bdf1e2f1ef
Divik Mathur <divikmathur1@gmail.com>
2025-02-24 17:12:18 +0530

    Update fuel.js

--- fuel.js
+++ fuel.js
@@ -4,6 +4,7 @@ document.querySelector('.geofs-ui-top').innerHTML = `
                 <div class="control-pad geofs-autopilot-pad" title="Toggle autopilot on/off">
                     <div class="control-pad-label transp-pad">FUEL MASTER</div>
                 </div>
+   </div>
 
    <div class="control-pad geofs-control-status" title="Mouse controls on hold, press [M] to enable.">
                 <div class="control-pad-label orange-pad">MOUSE OFF</div>
